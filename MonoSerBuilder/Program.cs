6652810a88889abcef4db33c1c334e7d7d6ea187
Nilav <nilavghosh@gmail.com>
2015-06-24 11:28:30 +0530

    Update Program.cs

--- MonoSerBuilder/Program.cs
+++ MonoSerBuilder/Program.cs
@@ -13,7 +13,6 @@ namespace MonoSerBuilder
             var model = TypeModel.Create();
             var type = Type.GetType("MonoDto.OrderHeader, MonoDto");
             model.Add(type, true);
-            int i = 1;
             type = Type.GetType("MonoDto.OrderDetail, MonoDto");
             model.Add(type, true);
             model.Compile("OrderSerializer", "MonoDtoSerializer.dll");
